f0ba59f7b3620b3a456665f6364b94aaacbc1aff
Chris Buguet <christophe.buguet@gmail.com>
2023-08-21 18:21:44 +0200

    Initial Commit

---
+++ SwiftDune/SwiftDuneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDuneApp.swift
+//  SwiftDune
+//
+//  Created by Christophe Buguet on 21/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDuneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
